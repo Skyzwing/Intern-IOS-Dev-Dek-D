3165444c2c139b8e0a0030d03d24d29d58a51e9c
surachatyaitammasan <59070180@it.kmitl.ac.th>
2019-05-23 14:08:00 +0700

    Initial Commit

---
+++ intern ios dek-d/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intern ios dek-d
+//
+//  Created by Surachat Yaitammasan on 23/5/2562 BE.
+//  Copyright © 2562 Surachat Yaitammasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
